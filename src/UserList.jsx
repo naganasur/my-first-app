8169369dd35c3112bdc5457bf644602de7111101
Abhishek Naganasur <abhisheknaganasur98@gmail.com>
2026-06-28 01:57:18 +0530

    Add error handling to UserList with try/catch pattern

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -3,23 +3,29 @@ import { useState, useEffect } from "react";
 function UserList() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError]= useState(null)
 
   useEffect(() => {
-    setTimeout(()=>{
+    
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((data) => {
         setUsers(data)
       setLoading(false);
-     });
-  },);
-}, []);
+     })
+     .catch((err) => {
+        setError("failed to load users. Please try again later.");
+        setLoading(false);
+    });
+
+
+  }, []);
 
   return (
     <div>
-        {loading?(
-            <p> loading users...</p>
-        ):(
+        {loading && <p> Loading users ...</p>}
+        {error && <p style={{color:"red"}}>{error}</p>}
+        {!loading && !error &&(
     <ul>
       {users.map((user) => (
         <li key={user.id}>{user.name}</li>
@@ -29,4 +35,5 @@ function UserList() {
     </div>
   );
 }
+
 export default UserList
